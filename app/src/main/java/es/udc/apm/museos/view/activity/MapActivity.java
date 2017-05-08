fc9b254cc2b913f9a7822e6ef1226ea322c3055e
4m1g0 <dev.4m1g0@gmail.com>
2017-05-08 20:42:43 +0200

    Test timertask to ask for devices and add test button to discover devices and show their RSSI on the console

--- app/src/main/java/es/udc/apm/museos/view/activity/MapActivity.java
+++ app/src/main/java/es/udc/apm/museos/view/activity/MapActivity.java
@@ -1,18 +1,26 @@
 package es.udc.apm.museos.view.activity;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 import es.udc.apm.museos.R;
 
@@ -37,6 +45,48 @@ public class MapActivity extends AppCompatActivity {
         if (!BTAdapter.isEnabled())
             BTAdapter.enable();
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                mTimerHandler.post(new Runnable() {
+                    public void run(){
+                        findRSSI();
+                    }
+                });
+            }
+        };
+
+        registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+
+        timer.schedule(task, 1, 10000);
     }
 
+    private void findRSSI() {
+        Log.d("TEST", "Buscando RSSI");
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("TEST", "receive");
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                int  rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                String name = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d("TEST", "Name: " + name  + device.getName() + " RSSID: " + device.getAddress() + "  RSSI: " + rssi + "dBm");
+            }
+        }
+    };
+
+    public void discoveryClick(View view) {
+        if (BTAdapter != null)
+        {
+            Log.d("TEST", "start discovery");
+            BTAdapter.startDiscovery();
+        }
+
+    }
 }
